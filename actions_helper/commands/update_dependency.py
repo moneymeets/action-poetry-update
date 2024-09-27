551c49f69a8cead9b69615ebd1dfcab03b01a603
Felix Hoffmann <felix.hoffmann@moneymeets.com>
2024-09-27 10:50:27 +0100

    WIP

--- actions_helper/commands/update_dependency.py
+++ actions_helper/commands/update_dependency.py
@@ -9,7 +9,8 @@ def run_process(command: str, check: bool = True, cwd: Optional[Path] = None):
 
 
 def update_package(revamp_dir: Path):
-    run_process("pwd")
-    run_process("pwd", cwd=revamp_dir)
+    # run_process("pwd")
+    # run_process("pwd", cwd=revamp_dir)
+    run_process("poetry run which python", cwd=revamp_dir)
     run_process("poetry env info", cwd=revamp_dir)
     run_process("poetry env list --full-path", cwd=revamp_dir)
