f53e1e35c4f8137f3520110f2cd92d8cf01c72d5
Felix Hoffmann <felix.hoffmann@moneymeets.com>
2024-09-27 10:20:45 +0100

    WIP

--- actions_helper/common/utils.py
+++ actions_helper/common/utils.py
@@ -1,12 +1,2 @@
-import subprocess
-from pathlib import Path
-from typing import Optional
-
-
-def run_process(command: str, check: bool = True, capture_output: bool = False, cwd: Optional[Path] = None):
-    print(f"RUNNING {command} - using {cwd}")
-    return subprocess.run(command, check=check, shell=True, text=True, capture_output=capture_output, cwd=cwd)
-
-
 def parse_bool(value: str) -> bool:
     return value.lower() in ["true", "t", "1"]
--- actions_helper/main.py
+++ actions_helper/main.py
@@ -2,10 +2,7 @@ from pathlib import Path
 
 import click
 
-from actions_helper.commands.update_dependency import get_packages_info, update_package
-from actions_helper.common.github_helpers import check_and_push_changes
-from actions_helper.common.templating import render_string_from_template
-from actions_helper.common.utils import parse_bool
+from actions_helper.commands.update_dependency import update_package
 
 
 @click.group()
@@ -14,45 +11,9 @@ def cli():
 
 
 @cli.command(name="dependency-update")
-@click.option("--dry-run", default="true", type=str)
-@click.option("--top-level", default="true", type=str)
-@click.option("--reviewers", default="", type=str)
 @click.option("--revamp-dir", default=Path.cwd(), type=click.Path(file_okay=False, exists=True, path_type=Path))
-def cmd_dependency_update(dry_run: str, reviewers: str, top_level: str, revamp_dir: Path):
-    # dry_run = parse_bool(dry_run)
-    dry_run = True
-    top_level = parse_bool(top_level)
-    reviewers = reviewers.split(",") if reviewers else []
-    packages = get_packages_info(revamp_dir=revamp_dir)
-    updated_packages = update_package(dry_run=dry_run, revamp_dir=revamp_dir)
-
-    rendered_message = render_string_from_template(
-        "description.j2",
-        context={
-            "updated_packages": [
-                updated_package
-                for updated_package in updated_packages
-                if updated_package.package_name in [package.package_name for package in packages]
-            ]
-            if top_level
-            else updated_packages,
-            "skipped": [
-                package
-                for package in packages
-                if package.package_name not in [updated_package.package_name for updated_package in updated_packages]
-            ],
-        },
-    )
-
-    if not dry_run:
-        print("Revamp: Commiting to GitHub...")
-        check_and_push_changes(
-            pr_body=rendered_message,
-            reviewers=reviewers,
-            cwd=revamp_dir,
-        )
-    else:
-        print(rendered_message)
+def cmd_dependency_update(revamp_dir: Path):
+    update_package(revamp_dir=revamp_dir)
 
 
 if __name__ == "__main__":
