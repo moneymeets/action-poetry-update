a824c014c1dd6e73d9e14497c3ab6d85ffc50c42
Abiodun Sotunde <abiodun.sotunde@moneymeets.com>
2024-10-02 13:10:31 +0200

    feat(action): fix subprocess run error for git check

--- actions_helper/common/utils.py
+++ actions_helper/common/utils.py
@@ -5,7 +5,7 @@ def run_process(command: str, check: bool = True, capture_output: bool = False):
     print(f"RUNNING {command}")
     process = subprocess.run(command, check=check, shell=True, text=True, capture_output=capture_output)
 
-    if process.returncode != 0:
+    if check and process.returncode != 0:
         print(process.stderr)
         process.check_returncode()
 
